af4339e26b372c6ba514ff065627368acb15ce3f
Matthew Capper <matthew.capper01@gmail.com>
2019-02-27 14:25:10 -0600

    tables exercises

---
+++ 3.3_tables_exercises.sql
@@ -0,0 +1,8 @@
+USE employees;
+SHOW TABLES;
+DESCRIBE employees;
+-- 5:employees contains ints, date, varchar, date, and enumerated strings
+-- 6: Numerals: dept_emp, as does current_dept_emp (emp_no),dept_emp (emp_no), dept_manager, employees, salaries, titles
+-- 7: Strings: departments (dept_no, dept_name), dept_emp, dept_manager, employees, titles
+-- 8: Dates: dept_emp, dept_manager, employees, salaries, titles
+-- 9: employees and departments are 
